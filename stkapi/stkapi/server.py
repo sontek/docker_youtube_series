c89fbf0628c6c91f2d1614b59e040df4d9c2229d
John Anderson <sontek@gmail.com>
2021-01-21 17:43:49 -0400

    All the files from video 1

---
+++ stkapi/stkapi/server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
+
---
+++ stkweb/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState, useEffect} from 'react';
+
+function App() {
+  const [message, setMessage] = useState('Hello Moon');
+  useEffect(async () => {
+    const response = await fetch('http://localhost:8000/');
+    const data = await response.json();
+    setMessage(data['message']);
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {message}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
